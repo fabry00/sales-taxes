e1c6e73f2eb4761b3c1cf7077b750970b112d7dc
Fabrizio Faustinoni <exfaff@gmv.es>
2016-08-05 10:30:06 +0200

    Added SalesTaxesFactory

---
+++ salestaxes-api/src/main/java/com/id/salestaxesapi/api/impl/SalesTaxesFactory.java
@@ -0,0 +1,19 @@
+package com.id.salestaxesapi.api.impl;
+
+import com.id.salestaxesapi.api.ISalesTaxesAPI;
+import com.id.salestaxesapi.api.ITaxesCalculator;
+import com.id.salestaxesapi.impl.taxes.TaxesCalculator;
+
+/**
+ * SalesTaxes Factory
+ * @author Fabrizio Faustinoni
+ */
+public class SalesTaxesFactory {
+    
+    public ISalesTaxesAPI create(){
+        // Create the calculator to inject
+        ITaxesCalculator calculator = new TaxesCalculator();
+        return new SalesTaxesAPI(calculator);
+    }
+    
+}
