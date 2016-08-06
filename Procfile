89cf2a36c1a688035945c68c373c2cc16b1a6537
fabry <fabry_00@hotmail.com>
2016-08-06 10:44:20 +0200

    Improved a bit REST GET and persistent

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -jar salestaxes-rest/target/salestaxes-rest-1.0-SNAPSHOT-launcher.jar server salestaxes-rest/service.yml
---
+++ salestaxes-api/src/main/java/com/id/salestaxesapi/obj/persistent/ElementExistsException.java
@@ -0,0 +1,14 @@
+package com.id.salestaxesapi.obj.persistent;
+
+/**
+ * Just an exception
+ *
+ * @author Fabrizio Faustinoni
+ */
+public class ElementExistsException extends Exception {
+
+    public ElementExistsException(String msg) {
+        super(msg);
+    }
+
+}
